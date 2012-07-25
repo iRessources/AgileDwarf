4ffd8299570616008d91dce72daffe535f4ece9f
Mark Ablovacky <mark.ablov@gmail.com>
2012-07-25 18:43:18 +0800

    Replace named scopes by methods for compatibility with Rails 3

--- app/models/sprints.rb
+++ app/models/sprints.rb
@@ -3,19 +3,14 @@ class Sprints < Version
 
   validate :start_and_end_dates
 
-  named_scope :open_sprints, lambda { |project|
-    {
-        :order => 'ir_start_date ASC, ir_end_date ASC',
-        :conditions => [ "status = 'open' and project_id = ?", project.id ]
-    }
-  }
-
-  named_scope :all_sprints, lambda { |project|
-    {
-        :order => 'ir_start_date ASC, ir_end_date ASC',
-        :conditions => [ "project_id = ?", project.id ]
-    }
-  }
+  class << self
+    def open_sprints(project)
+      scoped(:order => 'ir_start_date ASC, ir_end_date ASC', :conditions => [ "status = 'open' and project_id = ?", project.id ])
+    end
+    def all_sprints(project)
+      scoped(:order => 'ir_start_date ASC, ir_end_date ASC', :conditions => [ "project_id = ?", project.id ])
+    end
+  end
 
   def start_and_end_dates
     errors.add_to_base("Sprint cannot end before it starts") if self.ir_start_date && self.ir_end_date && self.ir_start_date >= self.ir_end_date
