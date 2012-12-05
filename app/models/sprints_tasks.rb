2fe8c22e3efc286ec8452c1394396f3ebe63a731
Ric Emery <ric.emery@gmail.com>
2012-12-05 14:13:13 -0700

    Add trackers to description in Tasks view

--- app/models/sprints_tasks.rb
+++ app/models/sprints_tasks.rb
@@ -21,8 +21,8 @@ class SprintsTasks < Issue
       user = User.current.id if user == 'current'
       cond << user
     end
-    SprintsTasks.find(:all, :select => 'issues.*, sum(hours) as spent', :order => SprintsTasks::ORDER, :conditions => cond, :group => "issues.id",
-                      :joins => [:status], :joins => "left join time_entries ON time_entries.issue_id = issues.id", :include => [:assigned_to]).each{|task| tasks << task}
+    SprintsTasks.find(:all, :select => 'issues.*, sum(hours) as spent, trackers.name AS t_name', :order => SprintsTasks::ORDER, :conditions => cond, :group => "issues.id",
+                      :joins => [:status], :joins => "left join time_entries ON time_entries.issue_id = issues.id left join trackers on trackers.id = tracker_id", :include => [:assigned_to]).each{|task| tasks << task}
     return tasks
   end
 
